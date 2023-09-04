5cd1a77d57581c41081cf249c43804e3b5d2b48f
TotalUmbrella <jerryruoxuzhang@gmail.com>
2023-09-04 19:58:15 +0800

    Added content stuff
    for home tab

--- script.js
+++ script.js
@@ -212,7 +212,69 @@ function handleChange(){
         console.log("clicked")
     }
 //Menubar end
+//MonthCalc
+function monthCalc(month){
+    if (month == 1){
+        return "Jan";
+    }
+    else if (month == 2){
+        return "Feb";
+    }
+    else if (month == 3){
+        return "Mar";
+    }
+    else if (month == 4){
+        return "Apr";
+    }
+    else if (month == 5){
+        return "May";
+    }
+    else if (month == 6){
+        return "Jun";
+    }
+    else if (month == 7){
+        return "Jul";
+    }
+    else if (month == 8){
+        return "Aug";
+    }
+    else if (month == 9){
+        return "Sep";
+    }
+    else if (month == 10){
+        return "Oct";
+    }
+    else if (month == 11){
+        return "Nov";
+    }
+    else if (month == 12){
+        return "Dec";
+    }
+}
 
+function dayCalc(day){
+    if (day == 1){
+        return "Mon";
+    }
+    else if (day == 2){
+        return "Tue";
+    }
+    else if (day == 3){
+        return "Wed";
+    }
+    else if (day == 4){
+        return "Thu";
+    }
+    else if (day == 5){
+        return "Fri";
+    }
+    else if (day == 6){
+        return "Sat";
+    }
+    else if (day == 0){
+        return "Sun";
+    }
+}
 
 // call function f1 every 1 second
 setInterval(main, 500);
